1ffd89babc23869b801d5cde74c9b1a615d87837
Sammmy22 <sambhavc225@outlook.com>
2023-01-26 00:23:39 +0530

    basic visualization

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const tiles = document.querySelectorAll(".tiles");
 
+let turn = 0;
 for (let i = 0; i < tiles.length; i++) {
   tiles[i].addEventListener("click", function () {
     // alert("Tile clicked");
+    turn++;
+
+    if (tiles[i].classList.contains("x")) {
+      console.log("This tile already has x tile");
+      turn--;
+    }
+
+    if (tiles[i].classList.contains("o")) {
+      console.log("This tile already has o tile");
+      turn--;
+    }
+
+    console.log(turn);
+    tiles[i].classList.add(turn % 2 == 0 ? "x" : "o");
   });
 }
