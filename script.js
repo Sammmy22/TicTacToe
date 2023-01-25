bc54e51b1191650385bb1d080128f574edfd0bd7
Sammmy22 <sambhavc225@outlook.com>
2023-01-25 20:40:59 +0530

    Added in tiles

---
+++ script.js
@@ -0,0 +1,7 @@
+const tiles = document.querySelectorAll(".tiles");
+
+for (let i = 0; i < tiles.length; i++) {
+  tiles[i].addEventListener("click", function () {
+    // alert("Tile clicked");
+  });
+}
