348c80488bb814fb0a0ec3c4dd8bff5bdf8f37fc
Sammmy22 <sambhavc225@outlook.com>
2023-01-28 21:47:39 +0530

    Completed with all the checks

--- script.js
+++ script.js
@@ -1,22 +1,90 @@
 const tiles = document.querySelectorAll(".tiles");
 
-let turn = 0;
+let counter = 0;
+
+let win = false;
 for (let i = 0; i < tiles.length; i++) {
   tiles[i].addEventListener("click", function () {
-    // alert("Tile clicked");
-    turn++;
+    const chance = document.querySelector("#chance");
 
-    if (tiles[i].classList.contains("x")) {
-      console.log("This tile already has x tile");
-      turn--;
+    document.querySelector("#reset").addEventListener("click", function () {
+      win = false;
+
+      if (tiles[i].classList.contains("X")) {
+        tiles[i].classList.remove("X");
+      }
+
+      if (tiles[i].classList.contains("O")) {
+        tiles[i].classList.remove("O");
+      }
+
+      counter % 2 == 0
+        ? (chance.innerHTML = "TURN: X")
+        : (chance.innerHTML = "TURN: O");
+    });
+
+    if (!win) {
+      if (
+        tiles[i].classList.contains("X") ||
+        tiles[i].classList.contains("O")
+      ) {
+        console.log("Already filled!");
+        tiles[i].removeEventListener("click", () => {});
+      } else {
+        tiles[i].classList.add(counter % 2 == 0 ? "X" : "O");
+        counter++;
+        counter % 2 == 0
+          ? (chance.innerHTML = "TURN: X")
+          : (chance.innerHTML = "TURN: O");
+      }
     }
 
-    if (tiles[i].classList.contains("o")) {
-      console.log("This tile already has o tile");
-      turn--;
+    if (
+      (tiles[0].classList.contains("X") &&
+        tiles[1].classList.contains("X") &&
+        tiles[2].classList.contains("X")) ||
+      (tiles[3].classList.contains("X") &&
+        tiles[4].classList.contains("X") &&
+        tiles[5].classList.contains("X")) ||
+      (tiles[6].classList.contains("X") &&
+        tiles[7].classList.contains("X") &&
+        tiles[8].classList.contains("X")) ||
+      (tiles[0].classList.contains("X") &&
+        tiles[4].classList.contains("X") &&
+        tiles[8].classList.contains("X")) ||
+      (tiles[1].classList.contains("X") &&
+        tiles[4].classList.contains("X") &&
+        tiles[7].classList.contains("X")) ||
+      (tiles[2].classList.contains("X") &&
+        tiles[4].classList.contains("X") &&
+        tiles[6].classList.contains("X"))
+    ) {
+      chance.innerHTML = "X wins";
+      win = true;
     }
 
-    console.log(turn);
-    tiles[i].classList.add(turn % 2 == 0 ? "x" : "o");
+    if (
+      (tiles[0].classList.contains("O") &&
+        tiles[1].classList.contains("O") &&
+        tiles[2].classList.contains("O")) ||
+      (tiles[3].classList.contains("O") &&
+        tiles[4].classList.contains("O") &&
+        tiles[5].classList.contains("O")) ||
+      (tiles[6].classList.contains("O") &&
+        tiles[7].classList.contains("O") &&
+        tiles[8].classList.contains("O")) ||
+      (tiles[0].classList.contains("O") &&
+        tiles[4].classList.contains("O") &&
+        tiles[8].classList.contains("O")) ||
+      (tiles[1].classList.contains("O") &&
+        tiles[4].classList.contains("O") &&
+        tiles[7].classList.contains("O")) ||
+      (tiles[2].classList.contains("O") &&
+        tiles[4].classList.contains("O") &&
+        tiles[6].classList.contains("O"))
+    ) {
+      chance.innerHTML = "O wins";
+      win = true;
+    }
   });
 }
